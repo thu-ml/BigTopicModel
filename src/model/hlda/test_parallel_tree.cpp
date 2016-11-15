b457586e064df58e94f52a197a6facc01dc9cb5c
Jianfei Chen <chrisjianfeichen@gmail.com>
2016-11-15 20:34:48 +0800

    Revised test parallel tree (unfinished)

--- src/model/hlda/test_parallel_tree.cpp
+++ src/model/hlda/test_parallel_tree.cpp
@@ -6,7 +6,27 @@
 #include <mpi.h>
 #include "glog/logging.h"
 #include "distributed_tree.h"
+#include "mpi_helpers.h"
+#include <random>
 using namespace std;
+
+struct Operation {
+    int type, id;
+};
+
+// TODO how to compare?
+/*std::vector<Operation> GenerateData(int num_layers, int num_documents) {
+    std::vector<Operation> data;
+    std::mt19937 generator;
+
+    std::vector<
+
+    for (int i = 0; i < num_documents; i++) {
+        int type = i == 0 ? 0 : static_cast<int>(generator() % 2);
+
+    }
+    return std::move(data);
+}*/
 
 int main(int argc, char **argv) {
     google::InitGoogleLogging(argv[0]);
@@ -22,7 +42,18 @@ int main(int argc, char **argv) {
     MPI_Comm_size(MPI_COMM_WORLD, &process_size);
     LOG(INFO) << process_id << ' ' << process_size;
 
-    {
+    /*{
+        int num_layers = 6;
+        int num_documents = 1000000;
+
+        // Generate testing data
+        auto data = GenerateData(num_layers, num_documents);
+
+        // Broadcast data
+
+    }*/
+
+    /*{
         DistributedTree tree(3, std::vector<double>{1.0, 0.5});
 
         auto PrintTree = [&]() {
@@ -69,7 +100,7 @@ int main(int argc, char **argv) {
             }
             MPI_Barrier(MPI_COMM_WORLD);
         }
-    }
+    }*/
 
     MPI_Finalize();
 }
