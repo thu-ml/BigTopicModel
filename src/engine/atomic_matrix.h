f6c07e36bdf39d0a7031f2c1409dc85b777964e2
Jianfei Chen <chrisjianfeichen@gmail.com>
2016-11-16 14:52:46 +0800

    Distributed AtomicMatrix

--- src/engine/atomic_matrix.h
+++ src/engine/atomic_matrix.h
@@ -11,22 +11,82 @@
 #include <stdexcept>
 #include <memory.h>
 #include <mutex>
+#include "publisher_subscriber.h"
+#include "buffer_manager.h"
+#include <omp.h>
+#include "glog/logging.h"
 
 template <class T>
 class AtomicMatrix {
 public:
+    struct TOnRecv {
+        TOnRecv(AtomicMatrix &m): m(m) {}
+        AtomicMatrix &m;
+
+        void operator() (const char *msg, size_t length) {
+            const int *data = reinterpret_cast<const int *>(msg) + 1;
+            size_t n = length / sizeof(int) - 1;
+            int src = *(data - 1);
+            if (src == m.pub_sub.ID())
+                return;
+
+            int max_c_index = -1;
+            for (size_t i = 0; i < n; i+=3)
+                max_c_index = std::max(max_c_index, data[i+1]);
+
+            m.IncreaseC(max_c_index + 1);
+
+            auto sess = m.GetSession(false);
+
+            for (size_t i = 0; i < n; i+=2) {
+                sess.Inc(data[i], data[i+1], data[i+2]);
+            }
+        }
+    };
+
     struct Session {
-        Session(AtomicMatrix &m):
-                m(m), lock(new boost::shared_lock<boost::shared_mutex>(m.mutex_)) {}
+        Session(AtomicMatrix &m, bool if_publish):
+                m(m), lock(new boost::shared_lock<boost::shared_mutex>(m.mutex_)),
+                buffer(m.buffer_manager.Get()), if_publish(if_publish) {
+            buffer.clear();
+        }
+
+        Session(Session &&from) noexcept :
+            m(from.m), lock(std::move(lock)),
+            buffer(std::move(buffer)), if_publish(if_publish) {
+        }
+
+        ~Session() {
+            if (if_publish && !buffer.empty()) {
+                m.pub_sub.Publish(reinterpret_cast<char *>(buffer.data()),
+                                  buffer.size() * sizeof(int), true);
+            }
+
+            m.buffer_manager.Free(std::move(buffer));
+        }
 
         AtomicMatrix &m;
         std::unique_ptr<boost::shared_lock<boost::shared_mutex>> lock;
+        std::vector<int> buffer;
+        bool if_publish;
 
         T Get(int r, int c) { return m.Get(r, c); }
-        void Inc(int r, int c) { m.Inc(r, c); }
-        void Inc(int r, int c, T delta) { m.Inc(r, c, delta); }
-        void Dec(int r, int c) { m.Dec(r, c); }
-        void Dec(int r, int c, T delta) { m.Dec(r, c, delta); }
+        void Inc(int r, int c) {
+            m.Inc(r, c);
+            buffer.push_back(r); buffer.push_back(c); buffer.push_back(1);
+        }
+        void Inc(int r, int c, T delta) {
+            m.Inc(r, c, delta);
+            buffer.push_back(r); buffer.push_back(c); buffer.push_back(delta);
+        }
+        void Dec(int r, int c) {
+            m.Dec(r, c);
+            buffer.push_back(r); buffer.push_back(c); buffer.push_back(-1);
+        }
+        void Dec(int r, int c, T delta) {
+            m.Dec(r, c, delta);
+            buffer.push_back(r); buffer.push_back(c); buffer.push_back(-delta);
+        }
         int GetR() { return m.GetR(); }
         int GetC() { return m.GetC(); }
         std::mutex* GetLock(int c) { 
@@ -43,7 +103,8 @@ public:
 
     AtomicMatrix(int R = 0, int C = 0)
             : _r_size(R), _c_size(C), _r_capacity(R), _c_capacity(C),
-              _data(new std::atomic<T> [R*C]) {
+              _data(new std::atomic<T> [R*C]),
+              on_recv(*this), pub_sub(true, on_recv) {
         for (int i=0; i<C; i++) column_mutexes.emplace_back(new std::mutex());
     }
 
@@ -51,8 +112,8 @@ public:
         delete[] _data;
     }
 
-    Session GetSession() {
-        return Session(*this);
+    Session GetSession(bool if_publish = true) {
+        return Session(*this, if_publish);
     }
 
     // Parallel and exclusive
@@ -87,7 +148,12 @@ public:
         }
         if (_c_size < newC) {
             boost::unique_lock<boost::shared_mutex> lock(mutex_);
-            if (_c_size < newC) _c_size = newC;
+            if (_c_size < newC) {
+                _c_size = newC;
+                int message[3] = {0, newC - 1, 0};
+                pub_sub.Publish(reinterpret_cast<char*>(message),
+                                3 * sizeof(int), true);
+            }
         }
     }
 
@@ -114,6 +180,10 @@ public:
 
         delete[] old_data;
         _c_size = permutation.size();
+    }
+
+    void Barrier() {
+        pub_sub.Barrier();
     }
 
 private:
@@ -166,6 +236,10 @@ private:
     std::vector<std::unique_ptr<std::mutex>> column_mutexes;
 
     boost::shared_mutex mutex_;
+
+    TOnRecv on_recv;
+    PublisherSubscriber<TOnRecv> pub_sub;
+    BufferManager<int> buffer_manager;
 };
 
 #endif //BIGTOPICMODEL_ATOMIC_MATRIX_H
