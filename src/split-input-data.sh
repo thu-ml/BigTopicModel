5755ef53d7406592668526021e6bbf4ce0d4a983
sproblvem <sproblvem@gmail.com>
2017-03-21 21:29:48 +0800

    add the split-input-data script

---
+++ src/split-input-data.sh
@@ -0,0 +1,8 @@
+split -d -C 1170000 nips.libsvm.train nips.libsvm.train.
+gzip nips.libsvm.train.*
+
+#split -d -C 60000000 nytimes.libsvm.train nytimes.libsvm.train.
+#gzip nytimes.libsvm.train.*
+
+#split -d -C 428630000 pubmed.libsvm.train pubmed.libsvm.train.
+#gzip pubmed.libsvm.train.*
